18d6dc34725b58dc4ea036cbae528c3419954f99
Elmira <zhan0222@mail.ru>
2022-12-15 16:11:26 +0600

    add tasks

---
+++ task25/Program.cs
@@ -0,0 +1,22 @@
+﻿/*  Напишите цикл, который принимает на вход два числа (A и B) 
+и возводит число A в натуральную степень B.
+
+3, 5 -> 243 (3⁵)
+2, 4 -> 16           */
+
+
+int A;
+Console.Write("Enter first number: ");
+int.TryParse(Console.ReadLine()!, out A);
+int B;
+Console.Write("Enter second number: ");
+int.TryParse(Console.ReadLine()!, out B);
+Console.Write($"The result of raising the first number to a natural power of the value of the second number will be: {Exp(A,B)}");
+int Exp(int A, int B)
+{
+int Numb = A;
+for (int i = 1; i < B; i++)
+Numb = Numb*A;
+return Numb;
+
+}
---
+++ task27/Program.cs
@@ -0,0 +1,22 @@
+﻿/* Напишите программу, которая принимает на вход число и выдаёт сумму цифр 
+в числе.
+
+452 -> 11
+82 -> 10
+9012 -> 12  */
+
+int A;
+Console.Write("Enter the number whose sum you want to know: ");
+int.TryParse(Console.ReadLine()!, out A);
+Console.Write($"The sum of all the values ​​of the number you entered is equal to {SumAllNumb(A)}");
+int SumAllNumb(int _A)
+{
+    int Numb = 0;
+    int NewNumb = _A;
+    while (NewNumb > 0)
+    {
+        Numb += NewNumb % 10;
+        NewNumb /= 10;
+    }
+    return Numb;
+}
